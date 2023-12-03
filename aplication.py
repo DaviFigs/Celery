9b710ef3c8236566aba9697151ffbae915033894
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-02 22:25:22 -0400

    .

---
+++ aplication.py
@@ -0,0 +1,6 @@
+import task as tk
+
+
+def sum_numbs(num1, num2):
+    result = tk.summer.delay(num1, num2)
+    return result
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import task as tk
+import aplication as ap
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-print(tk.summer.delay(5,8))
+@app.post('/sum')
+def sum_numbs(num1:int, num2:int):
+    final = ap.sum_numbs(num1, num2)
+    return {'result': final}
