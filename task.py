85ff94f467040b2dd64798d2b31f4f6ad7d224bc
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-06 21:43:34 -0400

    Installing django

--- task.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from time import sleep
-
-app = Celery(broker='redis://127.0.0.1:6379/0')
-
-@app.task(bind = True, )
-def summer(self, n1,n2):
-    sleep(4)
-    return n1+n2
