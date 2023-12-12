8290a0cc90212abc3b4bf8bac5c4618c137f3341
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-12 19:50:20 -0400

    Creating forms and

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.test, name="test"), 
+    path('forms/', views.main_form, name="forms"),
+    path("processing/", views.processing, name="processing"),
+    path('success/', views.succes, name='success')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http.response import HttpResponse
 from . import tasks as tk
 
-def test(request):
+def main_form(request):
     tk.test.delay()
     return render(request, 'forms.html')
+
+def succes(request):
+    return render(request, 'success.html')
+
+def processing(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        cpf = request.POST.get('cpf')
+        email = request.POST.get('email')
+
+        return redirect('success')
+
+    else:
+        return redirect('forms')
