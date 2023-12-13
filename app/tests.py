99902da30129c0968c62d57383705b910f0674de
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-13 14:05:29 -0400

    Making tests inn pillow image

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from PIL import Image,ImageFont, ImageDraw
+from datetime import date
+
+#data = (7cm, 14,4 cm)
+
+#name = (5,9 cm, 18,80 cm)
+
+def configure_image(name, cpf):
+    try:
+        data = date.today().strftime('%d/%m/%Y')
+        coord_data = (700,1235)
+        coord_name = (700, 1840)
+
+        image = Image.open(r'media/card.png')
+        #font
+
+        black_rgb =(0,0,0)
+        font = ImageFont.truetype()
+        draw = ImageDraw.Draw(image)
+
+        draw.text(coord_data, data,font=font, fill=black_rgb)
+        draw.text(coord_name, name,font=font, fill=black_rgb)
+        image.save(f'invite_for_{name}.png')
+        final_image = image
+        return final_image
+    
+    except Exception as e:
+        raise f'Something get wrong: {e}'
+    
+
+
+image = configure_image('carlos', '00000000')
+image.show()
