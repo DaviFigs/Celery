e458e22111eedf844104b8bda4dd6f192e458ef3
Davi Figueiredo <devdeoliveira06@gmail.com>
2024-01-19 20:22:12 -0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -68,10 +68,6 @@ DATABASES = {
 }
 
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -87,9 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-BR'
 
